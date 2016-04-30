765b3455e107f8c9c9d0d0172e6c74b2de34930c
Kevin Wittek <kevin.wittek@styracosoft.de>
2016-04-30 12:25:15 +0200

    + initial import

---
+++ src/main/java/com/groovycoder/SpringBootSpockTestingApplication.java
@@ -0,0 +1,12 @@
+package com.groovycoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpockTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpockTestingApplication.class, args);
+	}
+}
